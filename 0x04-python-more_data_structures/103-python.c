7f670ce2a6a7fbdbd21bdbcd586100eadeab59e9
SaintAdoga <adogasaint@gmail.com>
2023-03-15 13:49:07 -0700

    update python c

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <Python.h>
+#include <object.h>
+#include <listobject.h>
+#include <bytesobject.h>
 
 /**
  * print_python_bytes - Prints bytes information
@@ -7,15 +10,18 @@
  * @p: Python Object
  * Return: no return
  */
+
 void print_python_bytes(PyObject *p)
 {
 	char *string;
 	long int size, i, limit;
+	long int size;
+	int i;
+	char *trying_str = NULL;
 
 	printf("[.] bytes object info\n");
 	if (!PyBytes_Check(p))
-	{
-		printf("  [ERROR] Invalid Bytes Object\n");
+@@ -19,25 +23,16 @@ void print_python_bytes(PyObject *p)
 		return;
 	}
 
@@ -24,9 +30,14 @@ void print_python_bytes(PyObject *p)
 
 	printf("  size: %ld\n", size);
 	printf("  trying string: %s\n", string);
+	PyBytes_AsStringAndSize(p, &trying_str, &size);
 
 	if (size >= 10)
 		limit = 10;
+	printf("  size: %li\n", size);
+	printf("  trying string: %s\n", trying_str);
+	if (size < 10)
+		printf("  first %li bytes:", size + 1);
 	else
 		limit = size + 1;
 
@@ -38,32 +49,8 @@ void print_python_bytes(PyObject *p)
 		else
 			printf(" %02x", 256 + string[i]);
 
+		printf("  first 10 bytes:");
+	for (i = 0; i <= size && i < 10; i++)
+		printf(" %02hhx", trying_str[i]);
 	printf("\n");
 }
-
-/**
- * print_python_list - Prints list information
- *
- * @p: Python Object
- * Return: no return
- */
-void print_python_list(PyObject *p)
-{
-	long int size, i;
-	PyListObject *list;
-	PyObject *obj;
-
-	size = ((PyVarObject *)(p))->ob_size;
-	list = (PyListObject *)p;
-
-	printf("[*] Python list info\n");
-	printf("[*] Size of the Python List = %ld\n", size);
-	printf("[*] Allocated = %ld\n", list->allocated);
-
-	for (i = 0; i < size; i++)
-	{
-		obj = ((PyListObject *)p)->ob_item[i];
-		printf("Element %ld: %s\n", i, ((obj)->ob_type)->tp_name);
-		if (PyBytes_Check(obj))
-			print_python_bytes(obj);
-	}
