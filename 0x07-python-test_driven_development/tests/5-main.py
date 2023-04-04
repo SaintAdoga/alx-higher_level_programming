f1639af44f2a9f7a4ce7def1668f6850985ceb6a
SaintAdoga <adogasaint@gmail.com>
2023-04-04 11:46:55 -0700

    update text and py file

---
+++ 0x07-python-test_driven_development/tests/5-main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+text_indentation = __import__('5-text_indentation').text_indentation
+
+text_indentation("""You can also use other symbols such as\
+        dots, spaces, or arrows to create different levels of indentation.\
+        The purpose of indentation is to visually organize and structure text, \
+        making it easier to read and understand.""")
